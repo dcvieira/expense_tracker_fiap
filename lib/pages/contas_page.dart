c39da2397e429e85194a2892741e253b893ab896
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2023-08-03 23:14:14 -0300

    initial commit

---
+++ lib/pages/contas_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ContasPage extends StatefulWidget {
+  const ContasPage({super.key});
+
+  @override
+  State<ContasPage> createState() => _ContasPageState();
+}
+
+class _ContasPageState extends State<ContasPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Contas Page'),
+    );
+  }
+}
